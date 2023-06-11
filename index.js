11005d503c787251445ddc966154d9e434bb5cbf
FallenHunter47 <98681536+FallenHunter47@users.noreply.github.com>
2023-06-12 00:00:05 +0530

    t_website/

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var parallaxContainer = document.querySelector('.parallax-container');
+//     var parallaxInstance = new Parallax(parallaxContainer, {
+//       relativeInput: true,
+//       hoverOnly: true
+//     });
+//   });
+  
